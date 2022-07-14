804abdaae136651eaa2a2a1d755dd7b583ca9e56
jmjmfaria <jmjmfaria@gmail.com>
2022-07-14 17:43:18 +0100

    Commit1

---
+++ src/components/Questions.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Questions.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,27 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service');
+const bundleTracker = require('webpack-bundle-tracker');
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    publicPath: "http://127.0.0.1:8080",
+    outputDir: "./dist/",
+
+    // --------------------------
+    devServer: {
+        devMiddleware: {
+            headers: {"Access-Control-Allow-Origin": "*"},
+            publicPath: "http://127.0.0.1:8080",
+        },
+        hot: 'only',
+
+    },
+    // ---------------------------
+    chainWebpack: config => {
+        config.optimization.splitChunks(false)
+        config.plugin('BundleTracker').use(bundleTracker, [
+            {
+                filename: './webpack-stats.json'
+            }
+        ])
+        config.resolve.alias.set('__STATIC__', 'static')
+    }
 })
